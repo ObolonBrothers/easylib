fe4ae66737d32d2addd459057fb08370f377efc8
flystyle <alex.syrotenko.official@gmail.com>
2017-03-29 22:57:04 +0300

    issue #1 : implemented classic one-side directed linked list

--- src/core/base/main/ClassicList.kt
+++ src/core/base/main/ClassicList.kt
@@ -1,7 +1,7 @@
 /**
  * Implementation of classic simple one-side linked list
  *
- * @author Alex Syrotenko (@flystyle).
+ * @author Alex Syrotenko (@FlyCreat1ve).
  * @since 0.1
  * @param E the type of elements which are consists in this data structure.
  * @see Node .
@@ -13,7 +13,7 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
     /**
      * Inner misc class for representation of container with data and next element reference.
      *
-     * @author Alex Syrotenko (@flystyle)
+     * @author Alex Syrotenko (@FlyCreat1ve)
      * @since 0.1
      * @param el valuable data object
      * @see ClassicList .
@@ -22,6 +22,7 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
      */
 
     class Node<T> (private val el: T) : Serializable {
+
         private var next : Node<T>? = null
 
         fun next(): Node<T>? = this.next
@@ -29,7 +30,7 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
         /**
          * Connect node with new element to list
          *
-         * @author Alex Syrotenko (@flystyle)
+         * @author Alex Syrotenko (@FlyCreat1ve)
          * @since 0.1
          * @param node new node with valuable data.
          * @see Node .
@@ -42,7 +43,7 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
         /**
          * Unwrap value of current node
          *
-         * @author Alex Syrotenko (@flystyle)
+         * @author Alex Syrotenko (@FlyCreat1ve)
          * @since 0.1
          * @see Node
          * @return Node value
@@ -57,11 +58,9 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (other?.javaClass != javaClass) return false
-
             other as Node<*>
 
             if (el != other.el) return false
-            if (next != other.next) return false
 
             return true
         }
@@ -76,75 +75,90 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
     /**
      * Iterator class for ClassicList
      *
-     * @author Alex Syrotenko (@flystyle)
+     * @author Alex Syrotenko (@FlyCreat1ve)
      * @since 0.1
-     * @param current reference to list`s first node
+     * @param head reference to list`s first node
      * @see ClassicList .
      * @constructor References first node for it`s inner tasks.
      */
 
-    class ClassicListIterator <E> (private var current : Node<E>?) : MutableIterator<E> {
+    class ClassicListIterator <E> (private var head: Node<E>?) : MutableIterator<E> {
+
+        private var current : Node<E>? = null
 
         /**
          * Check existence of next list node
          *
-         * @author Alex Syrotenko (@flystyle)
+         * @author Alex Syrotenko (@FlyCreat1ve)
          * @since 0.1
          * @return Boolean existence flag
          */
 
         override fun hasNext(): Boolean {
+            if (current == null && head !== null)
+                return true
             return current?.next() !== null
         }
 
         /**
          *
-         * @author Alex Syrotenko (@flystyle)
+         * @author Alex Syrotenko (@FlyCreat1ve)
          * @since 0.1
          * @return Next node value
          */
 
         override fun next() : E {
-            if (this.hasNext()) {
-                current = current!!.next()
-                return current!!.unwrap()
+            if (this.current == null) {
+                this.current = head
+                return this.current?.unwrap()!!
             }
-            else throw IndexOutOfBoundsException()
+
+            if (!this.hasNext()) {
+                throw IndexOutOfBoundsException()
+            }
+            current = current!!.next()
+            return current!!.unwrap()
         }
 
         /**
          *
          * Not implemented for current version.
          *
-         * @author Alex Syrotenko (@flystyle)
+         * @author Alex Syrotenko (@FlyCreat1ve)
          * @since 0.1.1
          * @see ClassicList
          * @see ClassicListIterator
          */
 
         override fun remove() {
-            // comes in 0.1.1 version
-            throw  UnsupportedOperationException()
+            // will come in 0.1.1 version
+            throw UnsupportedOperationException()
         }
     }
 
-    private var root : Node<E>? = null
-        get() = this.root
 
+    private var head : Node<E>? = null
     private var tail : Node<E>? = null
-        get() = this.tail
-
     private var _size = 0
 
     override val size: Int
         get() = this._size
 
 
+    /**
+     * Adds element of type E to this list
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param element Element which should be added to list
+     * @see ClassicList .
+     */
+
     override fun add(element: E): Boolean {
         val el = Node(element)
 
         if (tail == null) {
-            root = el
+            head = el
             tail = el
         }
         else {
@@ -155,21 +169,96 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
         return true
     }
 
+    /**
+     * Addition of all elements from other collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param elements Collection which contains elements with same type.
+     * @see ClassicList .
+     * @see add .
+     */
+
     override fun addAll(elements: Collection<E>): Boolean {
         elements.forEach { el -> this.add(el) }
         return true
     }
 
+    /**
+     * Remove element of type E from сurrent list, if it exists.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param element Element which should be removed from list.
+     * @see ClassicList .
+     */
+
     override fun remove(element: E): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(head == null) return true
+
+        if (head == tail) {
+            head = null
+            tail = null
+            _size--
+            return true
+        }
+
+
+        if (head?.unwrap()!! == element) {
+            head = head?.next()
+            _size--
+            return true
+        }
+
+        var t = head
+
+        while (t?.next() != null) {
+            if (t.next()?.unwrap() == element) {
+                if(tail == t.next()) {
+                    tail = t
+                }
+                t.next()?.next()?.let { t?.next()?.accept(it) }
+                _size--
+                return true
+            }
+            t = t.next()
+        }
+        return false
     }
+
+    /**
+     * Removes of all elements which are similar with some element from other collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param elements Collection which contains elements with same type.
+     * @see ClassicList .
+     * @see remove .
+     */
 
     override fun removeAll(elements: Collection<E>): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        elements.forEach { el -> this.remove(el) }
+        return true
     }
 
+    /**
+     * Check if element contains in this collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param element Element which are checking for existence in this collection.
+     * @see ClassicList .
+     */
+
     override fun contains(element: E): Boolean {
-        var temp = root
+
+        if (head?.unwrap()!! == element)
+            return true
+
+        if (tail?.unwrap()!! == element)
+            return true
+
+        var temp = head
         while (temp?.next() != null) {
             if (temp.unwrap()!! == element)
                 return true
@@ -178,28 +267,99 @@ class ClassicList<E> : MutableCollection<E>, Serializable {
         return false
     }
 
+    /**
+     * Check if all element of some collections contains in this collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @param elements Elements which are checking for existence in this collection.
+     * @see ClassicList .
+     */
+
     override fun containsAll(elements: Collection<E>): Boolean {
-        return if (elements.none { this.contains(it) }) false else false
+        return !elements.none { this.contains(it) }
     }
+
+    /**
+     * Check if this collection doesn`t have elements.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @see ClassicList .
+     */
 
     override fun isEmpty(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return this._size == 0
     }
+
+    /**
+     * Remove all objcets from collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @see ClassicList .
+     */
 
     override fun clear() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.head = null
+        this.tail = null
+        _size = 0
     }
+
+    /**
+     * Returns the iterator to the head element of collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @see ClassicList .
+     */
 
     override fun iterator(): MutableIterator<E> {
-        return ClassicListIterator(root)
+        return ClassicListIterator(head)
     }
 
+    /**
+     * Removes of all elements which are not present in other collection.
+     *
+     * @author Alex Syrotenko (@FlyCreat1ve)
+     * @since 0.1
+     * @see ClassicList .
+     */
+
     override fun retainAll(elements: Collection<E>): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val it = this.iterator()
+        while (it.hasNext()) {
+            val elem = it.next()
+            if (!elements.contains(elem)) {
+                this.remove(elem)
+            }
+        }
+        return true
     }
 
     override fun serialize(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // will come in version 0.2
+        TODO("not implemented")
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+
+        other as ClassicList<*>
+
+        if (!head!!.equals(other.head)) return false
+        if (!tail!!.equals(other.tail)) return false
+        if (_size != other._size) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = head?.hashCode() ?: 0
+        result = 31 * result + (tail?.hashCode() ?: 0)
+        result = 31 * result + _size
+        return result
+    }
+    
 }
--- src/core/base/test/ClassicListTest.kt
+++ src/core/base/test/ClassicListTest.kt
@@ -2,7 +2,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
- * @Author Alex Syrotenko (@flystyle)
+ * @Author Alex Syrotenko (@FlyCreat1ve)
  *  Created on 05.03.17.
  *  @See ClassicListTest
  *  @Testmethod addToClassicList. It tests fucntionality of element addition to list.
@@ -13,6 +13,8 @@ import org.junit.Test
 class ClassicListTest {
     private val list = ClassicList<Int>()
     private val sublist = ClassicList<Int>()
+    private val checklist = ClassicList<Int>()
+
 
     @Test fun addToClassicList() : Unit {
         val len = 20
@@ -35,6 +37,49 @@ class ClassicListTest {
         assertEquals(list.contains(30), false)
         assertEquals(list.containsAll(sublist), true)
 
+    }
+
+    @Test fun removeFromClassicList() : Unit {
+        val len = 20
+        for (i in IntRange(1, len))
+            list.add(i)
+
+        for (i in IntRange(1, len))
+            list.remove(i)
+
+        assertEquals(list.size, 0)
+    }
+
+    @Test fun clearList() : Unit {
+        val len = 20
+        for (i in IntRange(1, len))
+            list.add(i)
+
+        list.clear()
+        assertEquals(list.size, 0)
+    }
+
+    @Test fun filterTest() : Unit {
+        val len = 20
+        for (i in IntRange(1, len))
+            list.add(i)
+
+        println(list.filter { it -> it % 2 == 0 })
+    }
+
+    @Test fun retainsList() : Unit {
+        val len = 10
+        for (i in IntRange(1, len))         // 1, 2, ..., 9, 10
+            list.add(i)
+
+        for (i in IntRange(len/2, len)) {   // 6, 7, 8, 9, 11
+            sublist.add(i)
+            checklist.add(i)
+        }
+
+        list.retainAll(sublist)
+
+        assertEquals(list.equals(checklist), true)
     }
 
 }
